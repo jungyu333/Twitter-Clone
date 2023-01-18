92675424527ea0ae414e9772cc04790dff4d639b
jungyu333 <jungyu3826@naver.com>
2023-01-18 22:01:17 +0900

    feat : main page layout 구현 , favicon 적용완료

--- src/Routes/Main.tsx
+++ src/Routes/Main.tsx
@@ -1,7 +1,96 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const MainImage = styled.img`
+  width: 60vw;
+  height: 100vh;
+  min-width: 1000px;
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  width: 40vw;
+  min-width: 200px;
+  margin-left: 70px;
+  margin-bottom: 150px;
+  min-width: max-content;
+
+  & h1 {
+    font-size: 4rem;
+    font-weight: 900;
+    margin: 50px 0;
+    margin-top: 100px;
+  }
+
+  & h2 {
+    font-size: 2rem;
+    font-weight: 600;
+    margin: 20px 0;
+  }
+`;
+
+const Logo = styled.img`
+  width: 60px;
+  height: 60px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 400px;
+  min-width: max-content;
+  margin-top: 60px;
+
+  & button:first-child {
+    background-color: ${({ theme }) => theme.colors.main};
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  & button:last-child {
+    margin-top: 60px;
+  }
+`;
+
+const Button = styled.button`
+  margin: 12px 0;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 2px solid ${({ theme }) => theme.colors.contents};
+  padding: 15px 5px;
+  width: 100%;
+  min-width: max-content;
+  border-radius: 30px;
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors.main};
+  cursor: pointer;
+  font-weight: 600;
+`;
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <Wrapper>
+      <MainImage
+        src={`${process.env.PUBLIC_URL}/images/img_StartTwitter.png `}
+      />
+      <MainContainer>
+        <Logo src={`${process.env.PUBLIC_URL}/images/ic_logo.png `} />
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>오늘 트위터에 가입하세요.</h2>
+        <ButtonContainer>
+          <Button>가입하기</Button>
+          <Button>로그인</Button>
+          <Button>Google로 시작하기</Button>
+        </ButtonContainer>
+      </MainContainer>
+    </Wrapper>
+  );
 }
 
 export default Main;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import theme from './styles/theme';
+import { theme } from './styles/theme';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 
