352d6ab02907fb3c8391c61af0c465ad23b2fdba
jungyu333 <jungyu3826@naver.com>
2023-02-06 22:57:23 +0900

    feat : login 보호 router 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,15 +4,22 @@ import Main from 'Routes/Main';
 import Home from 'Routes/Home';
 import LogIn from 'Routes/LogIn';
 import LogOut from 'Routes/LogOut';
+import PrivateRoute from 'Routes/PrivateRoute';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/home" element={<Home />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+
         <Route path="/login" element={<LogIn />} />
-        <Route path="/logout" element={<LogOut />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/logout" element={<LogOut />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
