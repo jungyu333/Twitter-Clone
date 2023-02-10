1f4909e5611fe5eb6e4db87deecb6330dfeba86e
jungyu333 <jungyu3826@naver.com>
2023-02-10 15:17:32 +0900

    feat : Main Header 레이아웃 구현

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-icons/md';
 import NavButton from 'components/home/NavButton';
 import UserInfoButton from 'components/home/UserInfoButton';
+import { Link } from 'react-router-dom';
 
 const Wrapper = styled.div`
   display: flex;
@@ -20,6 +21,7 @@ const Header = styled.header`
   display: flex;
   flex-grow: 1.5;
   justify-content: flex-end;
+  width: 25%;
 `;
 
 const HeaderContainer = styled.div`
@@ -48,6 +50,12 @@ const NavigationBar = styled.nav`
 const Main = styled.main`
   display: flex;
   flex-grow: 4;
+  width: 60%;
+`;
+
+const MainContainer = styled.div`
+  width: 550px;
+  border-right: 1px solid ${({ theme }) => theme.colors.lightgray};
 `;
 
 const TweetButton = styled.div`
@@ -66,6 +74,57 @@ const TweetButton = styled.div`
 
 const NavigationContainer = styled.div`
   width: 100%;
+`;
+
+const MainHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.lightgray};
+
+  & h1 {
+    font-weight: 800;
+    font-size: 1.3rem;
+    width: 100%;
+    height: 50%;
+    padding: 10px;
+  }
+`;
+
+const TabContainer = styled.div`
+  display: flex;
+  height: 50%;
+  & a {
+    font-weight: 600;
+    width: 50%;
+    height: 100%;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.lightgray};
+    }
+  }
+`;
+
+const Tab = styled.span`
+  display: flex;
+  flex-direction: column;
+  width: max-content;
+  position: relative;
+`;
+
+const TabUnderBar = styled.span`
+  width: 100%;
+  height: 4px;
+  background-color: ${({ theme }) => theme.colors.contents};
+  position: absolute;
+  bottom: -100%;
+  border-radius: 10px;
+  margin-bottom: 1px;
 `;
 
 function Home() {
@@ -95,9 +154,25 @@ function Home() {
       </Header>
 
       <Main>
-        <div>
-          <div></div>
-        </div>
+        <MainContainer>
+          <MainHeader>
+            <h1>홈</h1>
+            <TabContainer>
+              <Link to={'/home'}>
+                <Tab>
+                  <span>For you</span>
+                  <TabUnderBar />
+                </Tab>
+              </Link>
+              <Link to={'/home'}>
+                <Tab>
+                  <span>Following</span>
+                  <TabUnderBar />
+                </Tab>
+              </Link>
+            </TabContainer>
+          </MainHeader>
+        </MainContainer>
       </Main>
     </Wrapper>
   );
