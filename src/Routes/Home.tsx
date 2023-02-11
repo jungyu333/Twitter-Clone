0770d016cbe5a2f2bd22ef4001bae3b91e9f0f83
jungyu333 <jungyu3826@naver.com>
2023-02-11 17:49:29 +0900

    feat : main header input에 내용이 들어오면 tweet 버튼이 활성화 되도록 구현 완료

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -158,7 +158,7 @@ const TweetButtonContainer = styled.span`
   justify-content: center;
 `;
 
-const InputTweetButton = styled.button`
+const InputTweetButton = styled.button<{ isInputFill: boolean }>`
   color: ${({ theme }) => theme.colors.white};
   background-color: ${({ theme }) => theme.colors.lightcontents};
   font-weight: 600;
@@ -166,18 +166,29 @@ const InputTweetButton = styled.button`
   padding: 8px 15px;
   border: none;
   border-radius: 30px;
+
+  cursor: ${({ isInputFill }) => (isInputFill ? 'pointer' : 'auto')};
+  background-color: ${({ theme, isInputFill }) =>
+    isInputFill ? theme.colors.contents : theme.colors.lightcontents};
 `;
 
 function Home() {
   const textRef = useRef<HTMLTextAreaElement>(null);
   const [maxLength, setMaxLength] = useState<number>(0);
+  const [isInputFill, setIsInputFill] = useState<boolean>(false);
 
   const calcLetterCount = useCallback(() => {
     if (textRef.current) {
       setMaxLength(textRef.current?.value?.length!);
+      if (textRef.current.value.length > 0) {
+        setIsInputFill(true);
+      } else {
+        setIsInputFill(false);
+      }
     }
   }, []);
 
+  console.log(isInputFill);
   const handleResizeHeight = useCallback(() => {
     if (textRef.current) {
       textRef.current.style.height = 'auto';
@@ -235,7 +246,12 @@ function Home() {
                 <TweetButtonContainer>
                   <LetterCount>{maxLength}</LetterCount>
 
-                  <InputTweetButton>Tweet</InputTweetButton>
+                  <InputTweetButton
+                    disabled={isInputFill}
+                    isInputFill={isInputFill}
+                  >
+                    Tweet
+                  </InputTweetButton>
                 </TweetButtonContainer>
               </InputBottom>
             </InputContainer>
