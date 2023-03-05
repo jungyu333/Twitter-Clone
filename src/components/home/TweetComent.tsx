c05c04cadb925f6a8feda5ec1d51624dedc1a56c
jungyu333 <jungyu3826@naver.com>
2023-03-05 13:40:23 +0900

    feat : comment submit button 생성

--- src/components/home/TweetComent.tsx
+++ src/components/home/TweetComent.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+const Wrapper = styled.form`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   & textarea {
     width: 100%;
     padding: 10px 5px;
@@ -16,10 +20,44 @@ const Wrapper = styled.div`
   }
 `;
 
+const CommentSubmitButton = styled.button`
+  padding: 5px 10px;
+  background-color: ${({ theme }) => theme.colors.lightcontents};
+  color: ${({ theme }) => theme.colors.white};
+  border: none;
+  border-radius: 5px;
+  align-self: flex-end;
+  margin-top: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.contents};
+  }
+`;
+
 function TweetComent() {
+  const [commentText, setCommentText] = useState<string>('');
+
+  const onSubmitComment = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (commentText.length === 0) {
+      return;
+    } else {
+      console.log(commentText);
+    }
+    setCommentText('');
+  };
+  const onChangeCommentText = useCallback(
+    (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const text = event.target.value;
+      setCommentText(text);
+    },
+    [],
+  );
+
   return (
-    <Wrapper>
-      <textarea rows={2} />
+    <Wrapper onSubmit={onSubmitComment}>
+      <textarea rows={2} value={commentText} onChange={onChangeCommentText} />
+      <CommentSubmitButton>reply</CommentSubmitButton>
     </Wrapper>
   );
 }
