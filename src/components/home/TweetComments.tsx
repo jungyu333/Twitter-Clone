f5ebed763284635d4be486d17352dcf14e7f2c4e
jungyu333 <jungyu3826@naver.com>
2023-03-08 15:07:41 +0900

    refactor : tweet comment 부분 컴포넌트로 분리 & comment 작성 날짜 포맷팅

--- src/components/home/TweetComments.tsx
+++ src/components/home/TweetComments.tsx
@@ -4,63 +4,10 @@ import { loadComments } from 'redux/action/comment';
 import { RootState, useAppDispatch } from 'redux/store';
 import styled from 'styled-components';
 import { IComment, ITweet, ITweetCommentsProps } from 'types/home';
+import CommentItem from './CommentItem';
 
 const Wrapper = styled.div`
   width: 100%;
-`;
-
-const Avater = styled.img`
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  min-width: 40px;
-`;
-
-const CommentsContainer = styled.div`
-  display: flex;
-  width: 100%;
-  margin: 10px 0;
-  align-items: center;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.lightgray};
-  &:last-child {
-    border: none;
-  }
-`;
-
-const Comment = styled.div`
-  width: 100%;
-  margin: 5px 0px 5px 20px;
-`;
-
-const CommentHeader = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  justify-content: space-between;
-  & span {
-    color: ${({ theme }) => theme.colors.gray};
-    font-size: 0.7rem;
-  }
-`;
-
-const HeaderInfo = styled.div`
-  display: flex;
-  align-items: center;
-  & h1 {
-    font-weight: 600;
-    font-size: 0.9rem;
-    margin-right: 5px;
-  }
-  & h2 {
-    color: ${({ theme }) => theme.colors.gray};
-    font-size: 0.8rem;
-  }
-`;
-
-const Text = styled.div`
-  max-width: 500px;
-  word-break: break-all;
-  margin-top: 10px;
 `;
 
 function TweetComments({ tweetId }: ITweetCommentsProps) {
@@ -90,20 +37,7 @@ function TweetComments({ tweetId }: ITweetCommentsProps) {
       {clickTweet &&
         clickTweet.comments &&
         clickTweet.comments.map((comment: IComment) => (
-          <CommentsContainer key={comment.id}>
-            <Avater src={comment.avatar} />
-            <Comment>
-              <CommentHeader>
-                <HeaderInfo>
-                  <h1>{comment.name}</h1>
-                  <h2>{comment.email}</h2>
-                </HeaderInfo>
-
-                <span>{comment.createdAt}</span>
-              </CommentHeader>
-              <Text>{comment.text}</Text>
-            </Comment>
-          </CommentsContainer>
+          <CommentItem key={comment.id} {...comment} />
         ))}
     </Wrapper>
   );
